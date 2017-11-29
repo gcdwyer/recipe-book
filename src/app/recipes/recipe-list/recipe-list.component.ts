7f2040ecda5bca7ed853382de8a850e4b6d188b0
gcdwyer <gcdwyer@gmail.com>
2017-11-29 00:02:56 -0500

    started adding shopping list UI to add and delete

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    // name, description, imagePath
+    new Recipe('Recipe 1', 'Description 1', 'http://via.placeholder.com/350x150'),
+    new Recipe('Recipe 1', 'Description 1', 'http://via.placeholder.com/350x150')
+  ];
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model'
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Ingredient1', 4),
+    new Ingredient('Ingredient2', 7)
+  ];
 
   constructor() { }
 
