b61c4285175073b3662327c662c671b316dfbfc1
gcdwyer <gcdwyer@gmail.com>
2017-12-16 13:37:50 -0500

    created shopping list service to pass data between components

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,42 @@
-import { Recipe } from './recipe.model';
+import { EventEmitter, Injectable } from '@angular/core';
 
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     // name, description, imagePath
-    new Recipe('Recipe 1', 'Description 1', 'http://via.placeholder.com/350x150'),
-    new Recipe('Recipe 2', 'Description 2', 'http://via.placeholder.com/350x150')
+    new Recipe(
+      'Recipe 1', 
+      'Description 1', 
+      'http://via.placeholder.com/350x150', 
+      [
+        new Ingredient('Ing1', 1),
+        new Ingredient('Ing2', 5)
+      ]),
+    new Recipe(
+      'Recipe 2', 
+      'Description 2', 
+      'http://via.placeholder.com/350x150',
+    [
+      new Ingredient('Ing3', 2),
+      new Ingredient('Ing4', 6)
+    ])
   ];
+
+  constructor(private shoppingListService: ShoppingListService) {
+
+  }
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
