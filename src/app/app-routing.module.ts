c33c55c3b0986b9d6812006a75630bad610fed24
gcdwyer <gcdwyer@gmail.com>
2017-12-18 00:22:43 -0500

    more routing and comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,19 @@ import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 
 const appRoutes: Routes =[
   // pathMatch used to for blank path because all paths contain the blank
   { path: '', redirectTo: '/recipes', pathMatch: 'full'},
   // path displays recipes
   { path: 'recipes', component: RecipesComponent, children: [
+    // childen path of recipes
     { path: '', component: RecipeStartComponent },
+    { path: 'new', component: RecipeEditComponent },
+    // dynamic paramaters must be ordered last
     { path: ':id', component: RecipeDetailComponent },
+    { path: ':id/edit', component: RecipeEditComponent },
   ] },
   // path deisplays shopping list
   { path: 'shopping-list', component: ShoppingListComponent },
