4c1c30e6649105f9a02f0b259e22c414c0c8cec2
gcdwyer <gcdwyer@gmail.com>
2017-12-23 23:08:39 -0500

    populated fields with data on edit

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+
+import { RecipeService } from './../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,18 +12,51 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService) { 
+
+    }
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
-        // boolean to see if id exists
+        // sets edit mode to boolean
         this.editMode = params['id'] != null;
         console.log("editMode: " + this.editMode);
+        this.initializeForm();
       }
     );
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
+  private initializeForm() {
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescripiton = '';
+
+    // if in edit mode
+    if (this.editMode) {
+      // set recipe to recipe selected
+      let recipe = this.recipeService.getRecipe(this.id);
+      // assign values with recipe selected
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescripiton = recipe.description;
+    }
+
+    // pass this form into the DOM
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(recipeImagePath),
+      'description': new FormControl(recipeDescripiton)
+    });
   }
 
 }
